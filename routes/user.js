00a111ef4668b5e2d20c6111baeb4da92ecffe70
Siriwat <siriwatjaiyungyuen@gmail.com>
2020-08-31 14:07:22 +0700

    Add Login Path 😄

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET user listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
-router.get('/login',(req, res, next)=>{
-  res.send('hello')
+
+router.get('/login', (req, res, next) => {
+  let title = "เข้าสู่ระบบ|ผู้ประกาศงาน"
+  res.render('form/login', { title: title });
 })
 
 module.exports = router;
