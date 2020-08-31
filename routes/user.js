3e9df4b9fd3f01d53c192370f1eb31629e9a1ca6
Siriwat <siriwatjaiyungyuen@gmail.com>
2020-08-31 12:48:01 +0700

    change file name ☺️

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET user listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/login',(req, res, next)=>{
+  res.send('hello')
+})
 
 module.exports = router;
