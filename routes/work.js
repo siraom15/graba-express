4bad1e93f91321a7dacf245a08f906982e858ecc
Siriwat <siriwatjaiyungyuen@gmail.com>
2020-09-02 01:16:47 +0700

    Fixed bug and Add CSS 🎖

--- routes/work.js
+++ routes/work.js
@@ -3,11 +3,10 @@ var router = express.Router();
 var config = require('../database/config').mysql_pool;
 var moment = require('moment');
 moment.locale("th");
-
 router.get('/', (req, res, next) => {
     let province_data = require('../data/province.json');
     config.getConnection((err, connection)=>{
-        connection.query('SELECT * FROM work', (err, rows)=>{
+        connection.query('SELECT * FROM work w JOIN user u on w.user_id = u.id ORDER BY w.date_of_announce DESC', (err, rows)=>{
             if(err) throw err;
             res.render('work/work', { title: 'Works |งานทั้งหมด', province_data: province_data, card_data : rows });
         })
@@ -16,12 +15,12 @@ router.get('/', (req, res, next) => {
     
 });
 router.get('/info/:id', (req, res, next) => {
-
     config.getConnection((err, connection)=>{
-        connection.query('SELECT * FROM work WHERE id = ?', req.params.id, (err, rows)=>{
+        connection.query('SELECT * FROM work w JOIN user u on w.user_id = u.id WHERE w.id = ?', req.params.id, (err, rows)=>{
             if(err) throw err;
-            let dateOfWork = moment().format('llll')
-            res.render('work/info', { title: "ดูเพิ่มเติม : " + rows[0].infomation , card_data : rows, dateOfWork :  dateOfWork });
+            let dateOfWork = moment(rows[0].date_of_work).format('llll');
+            let dateOfAnnounce = moment(rows[0].date_of_announce).format('llll');
+            res.render('work/info', { title: "ดูเพิ่มเติม : " + rows[0].infomation , card_data : rows, dateOfWork :  dateOfWork, dateOfAnnounce : dateOfAnnounce });
         })
         connection.end();
     });
