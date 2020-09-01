f6c571472425bef20094d3a016f03aa1e3aa17be
Siriwat <siriwatjaiyungyuen@gmail.com>
2020-09-02 02:33:27 +0700

    Add express-session  and some secret😏

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session')
+var { secret_password, secret_session } = require('../database/secret');
 
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/user');
@@ -20,16 +22,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// create session
+app.set('trust proxy', 1)
+app.use(session({ 
+  secret: secret_session,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 } 
+}));
+
+
 app.use('/', indexRouter);
 app.use('/user', userRouter);
 app.use('/work', workRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
