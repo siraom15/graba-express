378b38ed0c8eb0fa817c8e8853eb929ad69f9402
Siriwat <siriwatjaiyungyuen@gmail.com>
2020-09-02 23:26:18 +0700

    Add connection module 😈

---
+++ database/connect.js
@@ -0,0 +1,31 @@
+var con = require('./config').mysql_pool;
+
+con.getConnection((err) => {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + con.threadId);
+})
+con.on('error', function (err) {
+    console.log('caught this error: ' + err.toString());
+});
+function handleDisconnect(conn) {
+    conn.on('error', function (err) {
+        if (!err.fatal) {
+            return;
+        }
+        if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+            throw err;
+        }
+
+        console.log('Re-connecting lost connection: ' + err.stack);
+
+        connection = mysql.createPool(conn.config);
+        handleDisconnect(connection);
+        connection.getConnection();
+    });
+}
+handleDisconnect(con);
+
+module.exports = con;
