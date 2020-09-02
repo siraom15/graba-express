00049048300399d835aabe07e5b897385f96b3fa
Siriwat <siriwatjaiyungyuen@gmail.com>
2020-09-02 21:34:11 +0700

    Update fixed bug by hard code !!! 😡

--- public/javascripts/th.js
+++ public/javascripts/th.js
@@ -1,35 +1,63 @@
  $(function(){
-    var uqid = $('#uqid').val();
+    var provinceObject1 =  $("#province_1");
+    var amphureObject1 =  $("#amphure_1");
+    var districtObject1 =  $("#district_1");
 
-    var provinceObject =  $("#province"+uqid);
-    var amphureObject =  $("#amphure"+uqid);
-    var districtObject =  $("#district"+uqid);
+    var provinceObject2 =  $("#province_2");
+    var amphureObject2 =  $("#amphure_2");
+    var districtObject2 =  $("#district_2");
     
     // on change province
-    provinceObject.on('change', function(){
+    provinceObject1.on('change', function(){
         var provinceId = $(this).val();
         console.log(provinceId);
-        amphureObject.html('<option value="">เลือกอำเภอ</option>');
-        districtObject.html('<option value="">เลือกตำบล</option>');
+        amphureObject1.html('<option value="">เลือกอำเภอ</option>');
+        districtObject1.html('<option value="">เลือกตำบล</option>');
         $.get('/api/get_amphure/' + provinceId, function(data){
             var result = data;
             $.each(result, function(index, item){
-                amphureObject.append(
+                amphureObject1.append(
                     $('<option></option>').val(item.id).html(item.name_th)
                 );
             });
         });
     });
- 
+    provinceObject2.on('change', function(){
+        var provinceId = $(this).val();
+        console.log(provinceId);
+        amphureObject2.html('<option value="">เลือกอำเภอ</option>');
+        districtObject2.html('<option value="">เลือกตำบล</option>');
+        $.get('/api/get_amphure/' + provinceId, function(data){
+            var result = data;
+            $.each(result, function(index, item){
+                amphureObject2.append(
+                    $('<option></option>').val(item.id).html(item.name_th)
+                );
+            });
+        });
+    });
     // on change amphure
-    amphureObject.on('change', function(){
+    amphureObject1.on('change', function(){
         var amphureId = $(this).val();
  
-        districtObject.html('<option value="">เลือกตำบล</option>');
+        districtObject1.html('<option value="">เลือกตำบล</option>');
         $.get('/api/get_district/' + amphureId, function(data){
             var result = data;
             $.each(result, function(index, item){
-                districtObject.append(
+                districtObject1.append(
+                    $('<option></option>').val(item.id).html(item.name_th)
+                );
+            });
+        });
+    });
+    amphureObject2.on('change', function(){
+        var amphureId = $(this).val();
+ 
+        districtObject2.html('<option value="">เลือกตำบล</option>');
+        $.get('/api/get_district/' + amphureId, function(data){
+            var result = data;
+            $.each(result, function(index, item){
+                districtObject2.append(
                     $('<option></option>').val(item.id).html(item.name_th)
                 );
             });
