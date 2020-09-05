92a0eef51ea9dc95ba86b83a28b32a269b7f62b6
Siriwat <siriwatjaiyungyuen@gmail.com>
2020-09-05 14:19:54 +0700

    Add 404 Error page / Update typing css / Make clear to work info

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {title : "ไม่พบหน้านี้"});
 });
 
 module.exports = app;
