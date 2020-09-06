0156607cd77504520f5bb255c034059dca02521d
Siriwat <siriwatjaiyungyuen@gmail.com>
2020-09-06 19:19:40 +0700

    Update View more function  xDDD🎤

--- public/javascripts/view_more.js
+++ public/javascripts/view_more.js
@@ -1,18 +1,18 @@
 $().ready(() => {
     let page = 1;
-    var btn =  $("#seemore_btn");
-    btn.click(() => {
-        console.log("Clicked");
-        ++page;
-        $.get('/work/view_more/' + page, function (data) {
-            console.log(this.btn);
-            // let txt = '<div class="d-flex justify-content-center" id="seemore"><button class="btn btn-dark" id="seemore_btn"><div class="spinner-border text-danger text-center" role="status"> </div>ดูเพิ่มเติม</button></div>';
-            // $("<div id='viewMore'></div>").insertAfter("#seemore");
-            $("#viewMore").append(data)
-            // $("#seemore").remove();
-            // $(txt).insertAfter("#viewMore");
-            // $("#viewMore").children()[0].focus();
-            this.btn =  $("#seemore_btn");
-        });
+    var nearToBottom = 100;
+    $(window).scroll(function () {
+        if ($(window).scrollTop() + $(window).height() > $(document).height() - nearToBottom) {
+            ++page;
+            $.get('/work/view_more/' + page, function (data) {
+                console.log(data);
+                $("#viewMore").append(data)
+                if(!data){
+                    $("#seemore_btn").hide();
+                }else{
+                    $("#seemore_btn").show();
+                }
+            });
+        }
     });
 })
