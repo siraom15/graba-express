cb2d6136ca204cb5221ed474554d77311bcbb512
Siriwat <siriwatjaiyungyuen@gmail.com>
2020-09-07 10:05:28 +0700

    Add CSS when press navbar btn and Add Modal before announce 😬

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,7 +1,15 @@
 $(function () {
-    $(document).scroll(function () {
-      var navbar = $(".fixed-top");
-      navbar.toggleClass('scrolled-dark', $(this).scrollTop() > navbar.height());
-      navbar.toggleClass('navbar-dark', $(this).scrollTop() > navbar.height());
-    });
+  var navbar = $(".fixed-top");
+  $(document).scroll(function () {
+
+    navbar.toggleClass('scrolled-dark', $(this).scrollTop() > navbar.height());
+    navbar.toggleClass('navbar-dark', $(this).scrollTop() > navbar.height());
   });
+  $("#navbar-toggler-btn-index").click(() => {
+    navbar.toggleClass('scrolled-dark')
+  })
+  $("#navbar-toggler-btn-other").click(() => {
+    navbar.toggleClass('navbar-dark')
+    navbar.toggleClass('scrolled-dark')
+  })
+});
